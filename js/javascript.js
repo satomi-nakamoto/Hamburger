1cf44cc862f0c813dbcb34e200d3ba6e31219616
satomi-nakamoto <64627738+satomi-nakamoto@users.noreply.github.com>
2020-08-14 21:38:20 +0900

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+  function myFunc() {
+    document.querySelector( '.sidebar' ).classList.toggle( 'is-open' );
+ }
+
+// function ImageUp() {
+//   window.open("menu.html","window1","min-width=328px,height= 100vh");
+// }
+
+// function myClick() {
+//   if (/Chrome/i.test(navigator.userAgent)) { window.close(); } else { window.open('about:blank', '_self').close(); }
+// }
+
